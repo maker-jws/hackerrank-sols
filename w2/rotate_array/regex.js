2ba977be503542cd762f36bc0c6432fd9db8fb74
wsjoshua <ws.joshua@gmail.com>
2021-07-27 10:38:27 -0500

    wrapped regex

--- w2/rotate_array/regex.js
+++ w2/rotate_array/regex.js
@@ -1,6 +1,13 @@
-let re1 = /(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)[A-Za-z]+$/;
-// match any of the listed options followed by characters (and not special characters)
-
-let re2 = /^([a, e, i, o, u]).*\1$/igm
+let re1 = /^([a, e, i, o, u]).*\1$/igm
 // check if first and last characters are matching vowels 
 // capture first characters that match from set, followed by any number with last char match value of capture
+
+let re2 = /(Mr\.|Mrs\.|Ms\.|Dr\.|Er\.)[A-Za-z]+$/;
+// match any of the listed options followed by characters (and not special characters)
+
+let re3 = /([0-9]+)/gm
+//return an array of all integers in a given string, excluding special characters
+
+// reference >> https://www.hackerrank.com/challenges/js10-regexp-1/topics/javascript-regex
+
+// future tutorials
